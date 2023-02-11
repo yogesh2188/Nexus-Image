e20dda2878d8a0c7080b143a1a9c3f6f65a2a2f1
RutujaPawal <rutujapawal177@gmail.com>
2023-02-11 10:50:56 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to xpertIT";
+		return "welcome to DevOps";
 	}
 
 	public static void main(String[] args) {
